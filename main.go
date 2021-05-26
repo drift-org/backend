a045d949e0751a1887db6029cd23b9e48bfea420
Kenzie Tee <teekenzie@berkeley.edu>
2021-05-25 20:01:09 -0700

    setup connection to mongo and fmt

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"github.com/drift-org/backend/database"
+	"fmt"
+	"os"
+
 	"github.com/drift-org/backend/helpers"
 	"github.com/drift-org/backend/routes"
 	"github.com/joho/godotenv"
+	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	err := godotenv.Load()
 	helpers.AlertError(err, "The .env file could not be found")
 
-	database.ConnectDB()
+	// Setup mgm default config
+	databaseURL := os.Getenv("MONGO_URL")
+	dbName := os.Getenv("MONGO_DBNAME")
+	err = mgm.SetDefaultConfig(nil, dbName, options.Client().ApplyURI(databaseURL))
+	helpers.AlertError(err, "There was an error connecting to the database")
+	fmt.Println("Successfully connected to Mongo")
+
 	routes.SetupRouter()
 }
