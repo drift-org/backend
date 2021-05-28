624cc6a040b3c7ace6c2df518a96b132c8977665
Aiden Szeto <aszeto35@gmail.com>
2021-05-27 20:52:46 -0700

    Expanded mgm user model

--- models/user_model.go
+++ models/user_model.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/kamva/mgm/v3"
+import (
+       "github.com/kamva/mgm/v3"
+       "go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	mgm.DefaultModel `bson:",inline"`
@@ -9,5 +12,10 @@ type User struct {
 	EmailAddress     string `bson:"email_address" json:"email_address" binding:"required"`
 	Password         string `bson:"passsword" json:"passsword" binding:"required"`
 	// University       string `bson:"university" binding:"required"`
-	Points int `bson:"points" json:"points"`
+	Points 		 int `bson:"points" json:"points"`
+	Username	 string `bson:"username" json:"username" binding:"required"`
+	Friends		 [0]primitive.ObjectID `bson:"friends" json:"friends"`
+	FriendRequests	 [0]primitive.ObjectID `bson:"friend_requests" json:"friend_requests"`
+	PastDrifts	 [0]primitive.ObjectID `bson:"past_drifts" json:"past_drifts"`
+	CurrentDrift	 primitive.ObjectID `bson:"current_drift" json:"current_drift"`
 }
