bd7a9d894bb34439946a1bae830b03fd567ed3e4
Aiden Szeto <aszeto35@gmail.com>
2021-05-28 18:53:16 -0700

    changed json and bson names to camel case

--- models/user_model.go
+++ models/user_model.go
@@ -9,13 +9,13 @@ type User struct {
 	mgm.DefaultModel `bson:",inline"`
 	Name             string `bson:"name" json:"name" binding:"required"`
 	Age              int    `bson:"age" json:"age" binding:"required"`
-	EmailAddress     string `bson:"email_address" json:"email_address" binding:"required"`
+	EmailAddress     string `bson:"emailAddress" json:"emailAddress" binding:"required"`
 	Password         string `bson:"passsword" json:"passsword" binding:"required"`
 	// University       string `bson:"university" binding:"required"`
 	Points 		 int `bson:"points" json:"points"`
 	Username	 string `bson:"username" json:"username" binding:"required"`
 	Friends		 []primitive.ObjectID `bson:"friends" json:"friends"`
-	FriendRequests	 []primitive.ObjectID `bson:"friend_requests" json:"friend_requests"`
-	PastDrifts	 []primitive.ObjectID `bson:"past_drifts" json:"past_drifts"`
-	CurrentDrift	 primitive.ObjectID `bson:"current_drift" json:"current_drift"`
+	FriendRequests	 []primitive.ObjectID `bson:"friendRequests" json:"friendRequests"`
+	PastDrifts	 []primitive.ObjectID `bson:"pastDrifts" json:"pastDrifts"`
+	CurrentDrift	 primitive.ObjectID `bson:"currentDrift" json:"currentDrift"`
 }
