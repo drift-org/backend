a0d72bc97de59324d33ea2ee7f4f775e817f2151
Kenzie Tee <teekenzie@berkeley.edu>
2021-05-28 23:25:40 -0700

    completed challenge, drift models

---
+++ models/challenge_model.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"github.com/kamva/mgm/v3"
+)
+
+type Challenge struct {
+	mgm.DefaultModel `bson:",inline"`
+	Longitude        int    `bson:"longitude" json:"longitude"`
+	Latitude         int    `bson:"latitude" json:"lattitude"`
+	Address          string `bson:"address" json:"address"`
+	TaskName         string `bson:"taskName" json:"taskName" binding:"required"`
+	Description      string `bson:"description" json:"description" binding:"required"`
+	Points           int    `bson:"points" json:"points" binding:"required"`
+}
---
+++ models/drift_model.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Drift struct {
+	mgm.DefaultModel `bson:",inline"`
+	Challenges       []primitive.ObjectID `bson:"challenges" json:"challenges" binding:"required"`
+	Prize            string               `bson:"prize" json:"prize" binding:"required"`
+	Points           int                  `bson:"points" json:"points" binding:"required"`
+	Group            primitive.ObjectID   `bson:"group" json:"group" binding:"required"`
+	Progress         int                  `bson:"progress" json:"progress" binding:"required"`
+}
