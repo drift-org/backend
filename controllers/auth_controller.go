41076ad8225327140527a51146fbc6c84781ac23
Brandon Li <brandon.li820@icloud.com>
2021-06-08 13:18:20 -0600

    undo change

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -60,7 +60,7 @@ func (ctrl *authController) Register(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Success"})
+	context.JSON(http.StatusOK, gin.H{"message": "Success", "user": body})
 }
 
 func (ctrl *authController) Login(context *gin.Context) {
