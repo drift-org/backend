cb63e46958a8bc4a6e73c4373c92341ddb6d3d24
Kenzie Tee <teekenzie@berkeley.edu>
2021-06-14 22:42:25 -0700

    added missed files

---
+++ helpers/challenge.go
@@ -0,0 +1,6 @@
+package helpers
+
+func FindChallenge(longitude float64, latitude float64, radius uint8) {
+	
+
+}
---
+++ helpers/db_indexes.go
@@ -0,0 +1,15 @@
+package helpers
+
+import (
+	"context"
+	"time"
+
+	"github.com/drift-org/backend/models"
+)
+
+func SetupIndexes() {
+	ctx, canc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer canc()
+	
+	models.ChallengeIndex(ctx)
+}
